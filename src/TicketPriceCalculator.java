50c9da63ea8d62c9993a1a9a771bc051f54e96c1
Anca <anca.lumezeanu@gmail.com>
2023-10-05 22:39:10 +0100

    Improved program

--- src/TicketPriceCalculator.java
+++ src/TicketPriceCalculator.java
@@ -1,36 +1,59 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class TicketPriceCalculator {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Cost price: ");
-        double costPrice = scanner.nextDouble();
+    public static void main(String[] args) throws IOException {
+        BufferedReader input;
+        PrintWriter output = new PrintWriter(new FileWriter("final.csv"));
 
-        System.out.print("Does duty need to be added? ");
-        String duty = scanner.next();
+        double exchangeRate = Double.parseDouble(args[0]);
 
-        switch (duty) {
-            case "yes":
-                costPrice = costPrice + costPrice * 0.1;
+        input = new BufferedReader(new FileReader(args[1]));
 
-            case "no:":
-              break;
+        StringBuilder finalLine = new StringBuilder(input.readLine()).append(",Exchange Rate,Ticket Price");
+        output.println(finalLine);
+
+        String inputLine;
+
+        for (int i = 1; i < args.length; i++) {
+            while ((inputLine = input.readLine()) != null) {
+                String[] fields = inputLine.split(",");
+                finalLine = new StringBuilder();
+
+                double costPrice = Double.parseDouble(fields[0]);
+                double freight = Double.parseDouble(fields[1]);
+                double margin = Double.parseDouble(fields[2]);
+                String duty = fields[3];
+
+                switch (duty) {
+                    case "y":
+                        costPrice = costPrice + costPrice * 0.1;
+
+                    case "n":
+                        break;
+                }
+
+                double priceA = (costPrice + freight) / exchangeRate;
+
+                double ticketPrice = (priceA + (priceA * margin) / 100) / 0.7;
+
+                if (ticketPrice % 1 == 0) {
+                    ticketPrice+= 0.99;
+                } else {
+                    ticketPrice = Math.ceil(ticketPrice) - 0.01;
+                }
+
+                for (int j = 0; j < 4; j++) {
+                    finalLine.append(fields[j]).append(",");
+                }
+
+                finalLine.append(exchangeRate).append(",");
+                finalLine.append(ticketPrice);
+
+                output.println(finalLine);
+            }
         }
 
-        System.out.print("Freight rate: ");
-        double freightRate = scanner.nextDouble();
-
-        System.out.print("Exchange rate: ");
-        double exchangeRate = scanner.nextDouble();
-
-        double priceA = (costPrice + freightRate) / exchangeRate;
-
-        System.out.print("Margin %: ");
-        int margin = scanner.nextInt();
-
-        double ticketPrice = (priceA + (priceA * margin) / 100) / 0.7;
-
-        System.out.println(ticketPrice);
+        output.close();
     }
 }
